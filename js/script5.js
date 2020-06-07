84297fcf248316be2af3d01962cf12026be99326
Nicola <nicolavalente88@gmail.com>
2020-06-07 22:18:29 +0200

    script 5

--- js/script5.js
+++ js/script5.js
@@ -4,8 +4,8 @@
 var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 //Crea due ID HTML uno rosso e uno verde.
-var rosso = document.getElementById("dispari");
-var verde = document.getElementById("pari");
+var rosso = document.getElementById("rosso");
+var verde = document.getElementById("verde");
 
 //Distingui i numeri pari da quelli dispari contenuti in array
 for (var count = 0; count < array.length; count++) {
